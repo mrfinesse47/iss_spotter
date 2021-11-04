336620b18e573c780308820d8f32d2ba5fd60c51
Kevin Mason <krmason2020@gmail.com>
2021-11-04 22:28:57 +0000

    accounted for error code in fetchMyIp

--- iss.js
+++ iss.js
@@ -10,12 +10,19 @@
 const request = require("request");
 const url = "https://api.ipify.org?format=json";
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = function(callback) {
   request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
+      return;
     }
-    //console.log(response.statusCode);
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
